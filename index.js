a6a3a662f09c3ef33deededfef82a3efda9ec09f
ANDES <andresmontealegregiraldo@gmail.com>
2018-05-04 11:54:14 -0500

    video 9 POST GET  MongoDB

--- index.js
+++ index.js
@@ -12,10 +12,25 @@ app.use(bodyParser.urlencoded({extended: false }));
 app.use(bodyParser.json());
 
 app.get('/api/product', (req,res) =>{
-res.send(200, {products:[]}) //array JSON con productos
+
+	Product.find({}, (err, products)=>{
+		if(err) return res.status.send({message:`Error al realizar la petición ${err}`})
+		if(!products)	 return res.status(404).send({message: `No existen productos`})
+		res.send(200, 	{products}	) //array JSON con productos	
+	})
+
 });
 
 app.get('/api/product/:productId', (req,res)=> {
+	let productId= req.params.productId	
+	Product.findById(productId, (err, product)=>{
+			if(err) return res.status.send({message:`Error al realizar la petición ${err}`})
+			if(!product)	 return res.status(404).send({message: `El producto no existe`})
+
+		res.status(200).send({product})
+		console.log(product)
+
+	})
 
 });
 
@@ -36,8 +51,7 @@ if (err) res.status(500). send({message: `Error al guardar en la base de datos $
 		res.status(200).send({product:productStored})
 
 	})
-	//console.log(req.body);
-	//res.status(200).send({message:'Producto se ha recibido V2'})
+
 });
 
 app.put('/api/product/:productId',(req,res)=>{
